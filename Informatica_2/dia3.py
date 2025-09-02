c8a24ca4d90821691c9d40a31f4b6617dca0da7f
Gyohandpa <gyohand.perdomo@udea.edu.co>
2025-09-02 12:18:04 -0500

    Inicio--Preguardado

---
+++ Informatica_2/dia3.py
@@ -0,0 +1,1 @@
+#No se tomaron notas de esta clase en Visual, se realizo todo en LucidChart
---
+++ Informatica_2/dia4.py
@@ -0,0 +1,13 @@
+#Github->Sistema controlador de versiones->Remoto->Nube
+#Git->Sistema de control de versiones->Local->Repositorio
+#GitHub Desktop->Interfaz grafica para Git --Visual --Intuitivo
+
+"""
+LOCAL
+carpeta(git init) --adición-> Stage(ejemplo.py) --commit-> Repositorio(Ejemplo.py[hash(numero ideantificador super largo)])
+        |              ^
+        | push         | pull
+        |              |
+        v              |
+             GitHub
+"""
